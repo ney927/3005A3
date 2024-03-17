ff489b4d8ed14951203c4a5d9d0f38b38011a9e6
Neyha Billing <78521142+ney927@users.noreply.github.com>
2024-03-17 17:07:48 -0400

    all files added

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import psycopg2
+
+#connect the to database
 conn = psycopg2.connect(
     database = 'A3',
     user = 'postgres',
@@ -10,25 +12,29 @@ conn = psycopg2.connect(
 conn.autocommit = True
 cursor = conn.cursor()
 
-
-def getStudents():
+# retrieves and displays all records from the student table
+def getAllStudents():
     cursor.execute("select * from students")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
+# Inserts a new student record into the students table.
 def addStudent(fname, lname, email, date):
     insertStudent = f"INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES ('{fname}', '{lname}', '{email}', '{date}');"
     cursor.execute(insertStudent)
 
+# Updates the email address for a student with the specified student_id.
 def updateStudentEmail(id, email):
     updateStudent = f"UPDATE students SET email = '{email}' WHERE student_id = {id}"
     cursor.execute(updateStudent)
 
+# Deletes the record of the student with the specified student_id.
 def deleteStudent(id):
     deleteStudent = f"DELETE FROM students WHERE student_id = {id}"
     cursor.execute(deleteStudent)
 
+# prints the menu of actions the user can do
 def menu():
     print("""\nMENU: 
 1) Get all Students 
@@ -40,63 +46,89 @@ or enter BYEBYE to exit the application\n""")
 
 
 exit = False
-menu()
-userInput = input()
+menu() #prints the menu
+userInput = input() #gets user input
+
+#calls the appropriate function based on the user input
 if (userInput == '1'):
-    getStudents()
+    getAllStudents()
 elif (userInput == '2'):
+    # ask the user for the details of the student you want to create
     fname = input ("Enter the new student's first name: ")
     lname = input ("Enter the new student's last name: ")
     email = input ("Enter the new student's email: ")
     date = input ("Enter the new student's enrollment date (YYYY-MM-DD): ")
     addStudent(fname, lname, email, date)
+
+    # print the record of the new student
     cursor.execute(f"SELECT * from students WHERE email = '{email}';")
     print("student created: ", cursor.fetchone())
 elif (userInput == '3'):
+    # ask the user for the details of the student you want to update
     id = input ("Enter the id of the student you want to update: ")
     email = input ("Enter the student's new email: ")
     updateStudentEmail(id, email)
+
+    # print the updated record of the student
     cursor.execute(f"SELECT * from students WHERE student_id = '{id}';")
-    print("student created: ", cursor.fetchone())
+    print("student updated: ", cursor.fetchone())
 elif (userInput == '4'):
+    # get the record id of the student you want to delete
     id = input ("Enter the id of the student you want to delete: ")
     cursor.execute(f"SELECT * from students WHERE student_id = '{id}';")
     student = cursor.fetchone()
     deleteStudent(id)
+
+    #print the deleted student record
     print("student deleted", student)
-elif (userInput == "BYEBYE"):
+elif (userInput == "BYEBYE"): 
     exit = True
 
+# while the user chooses not to exit, continue completing actions based on user inpu
 while (not exit):
     cont = input("enter c to continue... ")
 
-    menu()
-    userInput = input()
+    menu() #prints the menu
+    userInput = input() #gets user input
+
+    #calls the appropriate function based on the user input
     if (userInput == '1'):
-        getStudents()
+        getAllStudents()
     elif (userInput == '2'):
+        # ask the user for the details of the student you want to create
         fname = input ("Enter the new student's first name: ")
         lname = input ("Enter the new student's last name: ")
         email = input ("Enter the new student's email: ")
         date = input ("Enter the new student's enrollment date (YYYY-MM-DD): ")
         addStudent(fname, lname, email, date)
+
+        # print the record of the new student
         cursor.execute(f"SELECT * from students WHERE email = '{email}';")
         print("student created: ", cursor.fetchone())
     elif (userInput == '3'):
+        # ask the user for the details of the student you want to update
         id = input ("Enter the id of the student you want to update: ")
         email = input ("Enter the student's new email: ")
         updateStudentEmail(id, email)
+
+        # print the updated record of the student
         cursor.execute(f"SELECT * from students WHERE student_id = '{id}';")
-        print("student created: ", cursor.fetchone())
+        print("student updated: ", cursor.fetchone())
     elif (userInput == '4'):
+        # get the record id of the student you want to delete
         id = input ("Enter the id of the student you want to delete: ")
         cursor.execute(f"SELECT * from students WHERE student_id = '{id}';")
         student = cursor.fetchone()
         deleteStudent(id)
+
+        #print the deleted student record
         print("student deleted", student)
-    elif (userInput == "BYEBYE"):
+    elif (userInput == "BYEBYE"): 
         exit = True
 
 print("\nbyebye!")
 
 conn.close()
+
+
+
--- createDB.py
+++ createDB.py
@@ -1,5 +1,6 @@
 import psycopg2
 
+#connect to the database
 conn = psycopg2.connect(
     database = 'A3',
     user = 'postgres',
@@ -11,8 +12,10 @@ conn = psycopg2.connect(
 conn.autocommit = True
 cursor = conn.cursor()
 
+# delete the table if it already exists
 cursor.execute("DROP TABLE IF EXISTS students;")
 
+# command to create the table and the correct schema
 createTable = """CREATE TABLE IF NOT EXISTS students( 
     student_id  SERIAL PRIMARY KEY,
     first_name  VARCHAR(255)   NOT NULL,
@@ -20,14 +23,17 @@ createTable = """CREATE TABLE IF NOT EXISTS students(
     email   VARCHAR(255)   UNIQUE   NOT NULL,
     enrollment_date DATE
 );"""
-cursor.execute(createTable)
+# execute the createTable command
+cursor.execute(createTable) 
 
+# command to insert the initial data into the table
 insertDate = """INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
 ('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
 ('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
 ('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');"""
+# execute he insert data command
 cursor.execute(insertDate)
 
 print("students table created with initial data.")
 
-conn.close()
+conn.close() #close the connection to the database
